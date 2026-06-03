e2b74753fc38ea90f0e7198cc5ae80523d04dab7
NCL01 <niimaabu370@gmail.com>
2026-06-03 22:51:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Cisse DAO Voting Loaded");
+let yesVotes = 0;
+let noVotes = 0;
+
+function voteYes() {
+  yesVotes++;
+  document.getElementById("yes").innerText = yesVotes;
+}
+
+function voteNo() {
+  noVotes++;
+  document.getElementById("no").innerText = noVotes;
+}
